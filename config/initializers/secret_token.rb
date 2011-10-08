ca26b7e91bb4bd0e494339a6dcdfcacf2d36d661
Jesse Copeland <jesse.copeland@gmail.com>
2011-10-07 23:14:45 -1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TagFriends::Application.config.secret_token = 'd1a92580361d5fccfa60451ddfed0f98fea7e08d88dd127ffe1e9a5ac4dc00219f78e853d66439269484dad290e8b49a797072d30706aa7d3ad5b125ccedf89f'
